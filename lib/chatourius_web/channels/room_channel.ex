21d159169ed4a43c617f230b15c3d39ae9909a37
kamranhossain <kamran.hossain227@gmail.com>
2020-03-11 22:34:18 +0600

    handle messages from room channel server

--- lib/chatourius_web/channels/room_channel.ex
+++ lib/chatourius_web/channels/room_channel.ex
@@ -4,4 +4,10 @@ defmodule ChatouriusWeb.RoomChannel do
   def join("room", _payload, socket) do
     {:ok, socket}
   end
+
+  def handle_in("message:new", payload, socket) do
+    broadcast! socket, "message:new", %{user: payload["user"],  
+                                        message: payload["message"]}
+    {:noreply, socket}
+  end
 end
