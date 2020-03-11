0b9a4a97c1403bb58ce4ab9e6f5d4b99d66d7b9f
kamranhossain <kamran.hossain227@gmail.com>
2020-03-12 02:32:23 +0600

    verify and assign token to connect user socket

--- lib/chatourius_web/channels/user_socket.ex
+++ lib/chatourius_web/channels/user_socket.ex
@@ -15,8 +15,16 @@ defmodule ChatouriusWeb.UserSocket do
   #
   # See `Phoenix.Token` documentation for examples in
   # performing token verification on connect.
-  def connect(_params, socket, _connect_info) do
-    {:ok, socket}
+  def connect(%{"token" => user_id_token}, socket) do
+    case Phoenix.Token.verify(socket, 
+                              "user_id", 
+                              user_id_token, 
+                              max_age: 1000000) do
+      {:ok, user_id} ->
+        {:ok, assign(socket, :user_id, user_id)}
+      {:error, _reason} ->
+        :error
+    end
   end
 
   # Socket id's are topics that allow you to identify all sockets for a given user:
