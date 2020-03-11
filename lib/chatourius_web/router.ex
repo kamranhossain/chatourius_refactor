01dc643ae404736e83424da92567ef1ef30c493e
kamranhossain <kamran.hossain227@gmail.com>
2020-03-11 19:41:47 +0600

    config the router for coherence route and auth

--- lib/chatourius_web/router.ex
+++ lib/chatourius_web/router.ex
@@ -1,12 +1,23 @@
 defmodule ChatouriusWeb.Router do
   use ChatouriusWeb, :router
-
+  use Coherence.Router
+  
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug(Coherence.Authentication.Session)
+  end
+
+  pipeline :protected do
+    plug(:accepts, ["html"])
+    plug(:fetch_session)
+    plug(:fetch_flash)
+    plug(:protect_from_forgery)
+    plug(:put_secure_browser_headers)
+    plug(Coherence.Authentication.Session, protected: true)
   end
 
   pipeline :api do
@@ -15,12 +26,21 @@ defmodule ChatouriusWeb.Router do
 
   scope "/", ChatouriusWeb do
     pipe_through :browser
+  end
 
+  scope "/", ChatouriusWeb do
+    pipe_through(:protected)
+    # Add protected routes below
     get "/", PageController, :index
   end
 
-  # Other scopes may use custom stacks.
-  # scope "/api", ChatouriusWeb do
-  #   pipe_through :api
-  # end
+  scope "/" do
+    pipe_through(:browser)
+    coherence_routes()
+  end
+
+  scope "/" do
+    pipe_through(:protected)
+    coherence_routes(:protected)
+  end
 end
